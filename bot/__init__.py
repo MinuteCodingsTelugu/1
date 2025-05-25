10e76f59715dda27c323c0248672cea8ceffc2ec
MinuteCodingsTelugu <156009168+MinuteCodingsTelugu@users.noreply.github.com>
2025-05-25 18:15:50 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -549,11 +549,11 @@ if len(AUTHOR_NAME) == 0:
 
 AUTHOR_URL = environ.get("AUTHOR_URL", "")
 if len(AUTHOR_URL) == 0:
-    AUTHOR_URL = "https://t.me/WZML_X"
+    AUTHOR_URL = "PSPKBOTS"
 
 TITLE_NAME = environ.get("TITLE_NAME", "")
 if len(TITLE_NAME) == 0:
-    TITLE_NAME = "WZ-M/L-X"
+    TITLE_NAME = "PSPKBOTS"
 
 COVER_IMAGE = environ.get("COVER_IMAGE", "")
 if len(COVER_IMAGE) == 0:
@@ -561,7 +561,7 @@ if len(COVER_IMAGE) == 0:
 
 GD_INFO = environ.get("GD_INFO", "")
 if len(GD_INFO) == 0:
-    GD_INFO = "Uploaded by WZML-X"
+    GD_INFO = "Uploaded by PSPKBOTS"
 
 SAVE_MSG = environ.get("SAVE_MSG", "")
 SAVE_MSG = SAVE_MSG.lower() == "true"
